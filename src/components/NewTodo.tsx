3618700cd9e696a25cbdcc458da0ee56a04dbe5e
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-12-23 21:51:42 +0330

    Managing State TypeScript

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import classes from "./NewTodo.module.css";
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInput = useRef<HTMLInputElement>(null);
   const submitHandler = (event: React.FormEvent) => {
@@ -13,7 +14,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="text"> Todo text</label>
       <input type="text" id="text" ref={todoTextInput} />
       <button>Add Todo</button>
